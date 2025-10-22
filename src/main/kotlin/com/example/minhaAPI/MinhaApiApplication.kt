58c8b9ec929b43b8283a1df9ec4cb92e9a5baae0
unknown <teushiftz@gmail.com>
2025-10-21 21:23:39 -0300

    asa

---
+++ src/main/kotlin/com/example/minhaAPI/MinhaApiApplication.kt
@@ -0,0 +1,11 @@
+package com.example.minhaAPI
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MinhaApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<MinhaApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/minhaAPI/MinhaApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.minhaAPI
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MinhaApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
